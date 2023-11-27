176186f748e6c2558c6bb97a66ba224e8fb2bba4
Klevi Alliu <ka21abv@herts.ac.uk>
2023-11-27 13:28:27 +0000

    populating the BookConsoleView class

--- src/BookConsoleView.java
+++ src/BookConsoleView.java
@@ -1,2 +1,8 @@
 public class BookConsoleView {
+    public void printBookDetails(String title, String author, double price){
+        System.out.println("Book: ");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: £" + price);
+    }
 }
