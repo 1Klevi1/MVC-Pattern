efb0d5d84ac747d5c03c22ddf80abfba9b1c8fae
Klevi Alliu <ka21abv@herts.ac.uk>
2023-11-27 13:33:20 +0000

    adding methods in the BookMain class

--- src/BookMain.java
+++ src/BookMain.java
@@ -1,5 +1,18 @@
 public class BookMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Create a book instance
+        Book book = new Book("Software Architecture", "Donald Flyn", 39.99);
+        // Create a book view instance
+        BookConsoleView view = new BookConsoleView();
+        // Create a book controller instance
+        BookController controller = new BookController(book, view);
+        // Display the initial book details
+        controller.updateView();
+        // Update the book details and display the updated details
+        controller.setBookTitle("Software Engineering");
+        controller.setBookAuthor("Roger S. Pressman");
+        controller.setBookPrice(34.99);
+        controller.updateView();
     }
 }
